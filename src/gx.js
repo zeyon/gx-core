c2a80c5b71e95e560cec0dc22e820fa112e6e438
Sebastian Glonner <sebastian.glonner@zeyon.net>
2016-04-13 10:52:08 +0200

    ADD: Add polyfills for String.htmlSpecialChars and Array.findBy.

--- src/gx.js
+++ src/gx.js
@@ -16,3 +16,61 @@ if (gx == undefined)
 gx.version = '1.100';
 gx.core = {};
 gx.ui = {};
+
+/**
+ * Mootools extras.
+ *
+ */
+
+
+/**
+ * Escapes special (X)HTML characters "<", ">", "&" and the double quotation marks '"'.
+ *
+ * @return {String}
+ */
+if ( typeof String.prototype.htmlSpecialChars !== 'function' ) {
+  String.prototype.htmlSpecialChars = function () {
+    return this
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;');
+  };
+}
+
+/**
+ * Array.find(function).
+ *
+ * Find an item of an array by providing a function returning true if found.
+ *
+ * [{id:2}, {id:3}].findBy(function(item) {
+ *   return item.id === 3;
+ * }) === {id:3}
+ *
+ * Convenience arguments: If you provide string as first argument and a second
+ * argument you can find an item of an array with the provided key:value
+ *
+ * Example: This will give the same result as above:
+ *
+ * [{id:2}, {id:3}].findBy('id', 3) === {id:3}
+ *
+ */
+if ( typeof Array.prototype.findBy !== 'function' ) {
+  Array.prototype.findBy = function(mixed, value) {
+    var func = mixed;
+    if ( typeof mixed === 'string' ) {
+      func = function(item) {
+        return item[mixed] === value;
+      };
+    }
+
+    for ( var item, i = this.length - 1; i >= 0; i-- ) {
+      item = this[i];
+      if ( func(item) === true )
+        return item;
+    }
+
+    return null;
+  };
+}
